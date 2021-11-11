fd3d977bf7da5b3e7a0d59768fce0edc9fc760e5
samxos <samueleastwood1@gmail.com>
2021-11-11 13:59:11 -0600

    intial skeleton

--- sim1.py
+++ sim1.py
@@ -39,7 +39,6 @@ def read_args(self):
     if len(sys.argv) < 6:
         print("Usage: sim1.py [seed] [duration] [lanes] [customer arrival time] [customer service rate]\n"
               "Ex. sim1 789 1000 5 4 3")
-    else:
         sys.exit(-1)
 
 

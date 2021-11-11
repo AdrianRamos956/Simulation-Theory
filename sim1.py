4fdf64450944a6df00a8ceb7d2c3d8fcf93df1c6
samxos <samueleastwood1@gmail.com>
2021-11-11 13:58:14 -0600

    intial skeleton

--- sim1.py
+++ sim1.py
@@ -27,21 +27,20 @@ class Simulation:
     logfile = open('logfile.txt', 'a+')
     rand_seed = sys.argv[1]
     sim_duration_minutes = sys.argv[2]
-    num_checkoutlanes = sys.argv[3]
+    num_checkout_lanes = sys.argv[3]
     customer_arrival_rate = sys.argv[4]
     customer_service_rate = sys.argv[5]
 
-
-
     def start(self):
-        self.read_args(self)
+        pass
 
 
 def read_args(self):
     if len(sys.argv) < 6:
         print("Usage: sim1.py [seed] [duration] [lanes] [customer arrival time] [customer service rate]\n"
               "Ex. sim1 789 1000 5 4 3")
-
+    else:
+        sys.exit(-1)
 
 
 if __name__ == '__main__':

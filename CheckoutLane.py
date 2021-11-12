f064ee6431e44a502d88a9f1ed29fccfc018c9d9
madiyeater <89595403+madiyeater@users.noreply.github.com>
2021-11-12 17:06:15 -0600

    Added Features to checkout lane and added customer time waited calculation

--- CheckoutLane.py
+++ CheckoutLane.py
@@ -5,9 +5,31 @@ import queue
 
 
 class CheckoutLane:
-    num_customers = int()
-    customer_queue = queue.Queue()
 
     def __init__(self, number):
         self.number = number
+        self.num_customers = 0
+        self.total_customers = 0
+        self.customer_queue = [] * self.number
+    
+    def customer_exit(self,lane):
+        self.customer_queue[lane].pop(0)
+        self.num_customers -= 1
+
+    def customer_enter(self, customer)
+        self.num_customers += 1
+        self.total_customers += 1
+        self.customer_queue[customer.lane].append(customer)
+        
+    def average_customers_waiting(self):
+        ct = 0
+        for i in self.customer_queue:
+            if len(i) >= 1:
+                ct += len(i) - 1
+        return ct
+
+
+    
+    
+
 
--- Customer.py
+++ Customer.py
@@ -33,10 +33,14 @@ class Customer:
         file1.write("Time: %2f Customer %d enters check-out lane %d",%(self.time_in, self.number, self.lane))
         file1.close()
 
-    def log_out(self):
+    def log_out(self,time):
+        self.time_out = time
         file1 = open("log.txt", "a")
-        file1.write("Time: %2f Customer %d exits check-out lane %d",%(self.time_in, self.number, self.lane))
+        file1.write("Time: %2f Customer %d exits check-out lane %d",%(self.time_out, self.number, self.lane))
         file1.close()
+
+    def customer_time_waited_calc(self, time_when_at_front):
+        self.time_waited = time_when_at_front - self.time_in
 
 
         
